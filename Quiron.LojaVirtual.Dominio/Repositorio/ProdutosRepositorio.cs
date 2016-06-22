b1b8d4ac1b5ab13105d45411ba7d42ee7dd30bce
OJBlack <https://github.com/OJBlack>
2016-06-21 23:14:23 -0300

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EFdbContext contexto = new EFdbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return contexto.Produtos; }
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+
+        private ProdutosRepositorio _repositorio;
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            var produto = _repositorio.Produtos.Take(10);
+
+            return View(produto);
+        }
+    }
+}
