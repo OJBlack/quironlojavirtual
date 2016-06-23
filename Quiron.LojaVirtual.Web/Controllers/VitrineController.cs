5da011e5212f2940228be0e8418ea7cc2bc3646c
OJBlack <https://github.com/OJBlack>
2016-06-22 23:24:57 -0300

    Paginação e Itens

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,24 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        public int ProdutosPorPagina = 3;
+        // GET: Vitirne
+        public ActionResult LsitaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                                .OrderBy(p => p.Descricao)
+                                .Skip((pagina - 1) * ProdutosPorPagina)
+                                .Take(ProdutosPorPagina);
+                            
+            return View(produtos);
+        }
+    }
+}
